d0b6709217c84c2a389f18a2a8192cc2a9670316
adriaandb <adriaan.debelder@gmail.com>
2022-01-31 19:23:38 +0100

    Initial commit

---
+++ mod_vertalers.py
@@ -0,0 +1,25 @@
+de_vertalers_antwerpen={
+
+" Bertha Heinz" :{ moedertaal : "nederlands"  , talen :[Frans,Duits,Spaans,Nederlands] ,  beschikbaar : "nee"},
+" Olga Jansen" :{ moedertaal : "nederlands"  , talen :[Frans,Duits,Spaans,Nederlands] ,  beschikbaar : "nee"},
+" Peter Jansen" :{ moedertaal : "nederlands"  , talen :[Frans,Duits,Spaans,Nederlands] ,  beschikbaar : "nee"},
+" Juila Gonzales" :{ moedertaal : "nederlands"  , talen :[Frans,Duits,Spaans,Nederlands] ,  beschikbaar : "nee"},
+" Roberto Di Carlo" :{ moedertaal : "nederlands"  , talen :[Frans,Duits,Spaans,Nederlands] ,  beschikbaar : "nee"},
+" Brigitte Dewinter" :{ moedertaal : "nederlands"  , talen :[Frans,Duits,Spaans,Nederlands] ,  beschikbaar : "nee"},
+" Mohammed El Hamza" :{ moedertaal : "nederlands"  , talen :[Frans,Duits,Spaans,Nederlands] ,  beschikbaar : "nee"},
+" Yusef El Hamza" :{ moedertaal : "nederlands"  , talen :[Frans,Duits,Spaans,Nederlands] ,  beschikbaar : "nee"},
+" Fatima El Hamza" :{ moedertaal : "nederlands"  , talen :[Frans,Duits,Spaans,Nederlands] ,  beschikbaar : "nee"}
+}
+
+
+
+de_vertalers_limburg={
+
+" Jan Peters" :{ moedertaal : "nederlands"  , talen :[Frans,Duits,Spaans,Nederlands] ,  beschikbaar : "nee"},
+" Claudio Da Franco" :{ moedertaal : "nederlands"  , talen :[Frans,Duits,Spaans,Nederlands] ,  beschikbaar : "nee"},
+" Nadia Hermandez" :{ moedertaal : "nederlands"  , talen :[Frans,Duits,Spaans,Nederlands] ,  beschikbaar : "nee"},
+" Carlos Hermandez" :{ moedertaal : "nederlands"  , talen :[Frans,Duits,Spaans,Nederlands] ,  beschikbaar : "nee"},
+" Jean-Marie Du Jardin" :{ moedertaal : "nederlands"  , talen :[Frans,Duits,Spaans,Nederlands] ,  beschikbaar : "nee"},
+" Fatima El Hamza" :{ moedertaal : "nederlands"  , talen :[Frans,Duits,Spaans,Nederlands] ,  beschikbaar : "nee"}
+
+}
